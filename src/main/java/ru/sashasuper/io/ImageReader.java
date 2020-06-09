4f2cd76fc757f77158c85c695b9015630333acba
alexasteppe <kemilme140@gmail.com>
2020-06-09 23:47:15 +0700

    Fix Image Reader

--- src/main/java/ru/sashasuper/io/ImageReader.java
+++ src/main/java/ru/sashasuper/io/ImageReader.java
@@ -1,5 +1,7 @@
 package ru.sashasuper.io;
 
+import ru.sashasuper.logic.Vector;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -7,13 +9,32 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class ImageReader {
-    public BufferedImage readImage(String fileName) throws IOException {
-        BufferedImage image = null;
+    private ImageReader() {
+    }
 
+    public static BufferedImage readImage(String fileName) throws IOException {
         File f = new File(fileName);
-        if(!f.canRead()) throw new FileNotFoundException(fileName);
-        image = ImageIO.read(f);
+        if(!f.exists() || !f.canRead())
+            throw new FileNotFoundException("Doesn't exists: " + fileName);
 
-        return image;
+        return ImageIO.read(f);
+    }
+
+    // Запись построчная. Значения [0;255]. Цветные изображения обесцвечиваются
+    public static Vector vectorFromImage(BufferedImage image) {
+        int h = image.getHeight(), w = image.getWidth();
+        float[] vectorArray = new float[h * w];
+        Vector v = new Vector(vectorArray.length, vectorArray);
+
+        int vectorIndex = 0;
+        for (int y = 0; y < h; y++) {
+            for (int x = 0; x < w; x++) {
+                int pixel = image.getRGB(x, y);
+                vectorArray[vectorIndex++] = (((pixel >> 16) & 0xFF) + ((pixel >> 8) & 0xFF) + (pixel & 0xFF)) / 3.0f;
+            }
+        }
+
+        return v;
     }
 }
+
