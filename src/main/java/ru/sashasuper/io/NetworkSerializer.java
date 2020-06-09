4c4b8d0a2abf4753c08c09b7e666ef87b6b19d11
alexasteppe <kemilme140@gmail.com>
2020-06-09 17:20:24 +0700

    Add Network Serializer

---
+++ src/main/java/ru/sashasuper/io/NetworkSerializer.java
@@ -0,0 +1,30 @@
+package ru.sashasuper.io;
+import ru.sashasuper.logic.Network;
+
+import java.io.*;
+
+public class NetworkSerializer {
+
+    public static void serialization(String fileName, Network net) throws IOException {
+        FileOutputStream outputStream = new FileOutputStream(fileName);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+
+        objectOutputStream.writeObject(net);
+
+        outputStream.close();
+
+    }
+
+    public static Network deserialization(String fileName) throws IOException, ClassNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream(fileName);
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+        Network network = (Network) objectInputStream.readObject();
+        fileInputStream.close();
+
+        return network;
+    }
+}
+
+
+
