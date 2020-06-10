717bb5f0b9d03242d98ee6cee06f4e3ef5e73887
lastblood <lastblood.xr@gmail.com>
2020-06-11 02:12:26 +0700

    Add asserts in Matrix and Vector constructors

--- src/main/java/ru/sashasuper/logic/Matrix.java
+++ src/main/java/ru/sashasuper/logic/Matrix.java
@@ -2,11 +2,17 @@ package ru.sashasuper.logic;
 
 import java.io.Serializable;
 
+import static ru.sashasuper.utils.Assertions.thr;
+
 public class Matrix implements Serializable {
     private int rows, columns;
     private float[][] values;
 
     public Matrix(int rows, int columns, float[][] values) {
+        thr(rows <= 0 || columns <= 0);
+        thr(values.length != rows);
+        for(int i = 0; i < rows; i++) thr(values[i].length != columns);
+
         this.rows = rows;
         this.columns = columns;
         this.values = values;
