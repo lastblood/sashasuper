45f88be373c8187cd1012b41a0bffb66b5fa1999
alexasteppe <kemilme140@gmail.com>
2020-06-10 22:46:13 +0700

    Update Matrix Vector Multiplication and vice versa

--- src/main/java/ru/sashasuper/logic/Math.java
+++ src/main/java/ru/sashasuper/logic/Math.java
@@ -20,28 +20,48 @@ public class Math {
             }
             result[i] = res;
         }
-        return new Vector(rows,result);
+        return new Vector(rows, result);
 
     }
 
-    public static float[] vectorMatrix(float[] vector, float[][] matrix) {
+    public static Vector mult(Vector firstVector, Vector secondVector) {
 
-        int rows = matrix[0].length;
-        int columns = matrix.length;
+        thr(firstVector.getLen() != secondVector.getLen());
 
-        float[] result = new float[rows];
-
-        if (rows != columns) return null;
-
-        for (int i=0; i<rows; i++) {
-            float res = 0;
-            for (int j=0; j < columns; j++) {
-                res += vector[j] * matrix[j][i];
-            }
-            result[i] = res;
-        }
+        Vector result = new Vector(firstVector.getLen(), new float[firstVector.getLen()]);
+        for (int i = 0; i < result.getLen(); i++)
+            result.getValues()[i] = firstVector.getValues()[i] * secondVector.getValues()[i];
         return result;
     }
 
+    public static Vector sub(Vector firstVector, Vector secondVector) {
 
+        thr(firstVector.getLen() != secondVector.getLen());
+
+        Vector result = new Vector(firstVector.getLen(), new float[firstVector.getLen()]);
+        for (int i = 0; i < result.getLen(); i++)
+            result.getValues()[i] = firstVector.getValues()[i] - secondVector.getValues()[i];
+        return result;
+
+    }
+
+    public static Matrix vMult(Vector column, Vector row) {
+
+        int rows = row.getLen();
+        int columns = column.getLen();
+
+        thr(columns != rows);
+
+        float[][] result = new float[columns][rows];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                result[i][j] = column.getValues()[i] * row.getValues()[j];
+            }
+        }
+        return new Matrix(rows, columns, result);
+    }
 }
+
+
+
+
