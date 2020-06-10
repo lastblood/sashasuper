7762e8b64a1c58c1b0be00232ca6e72d30dbbe34
lastblood <lastblood.xr@gmail.com>
2020-06-10 21:47:00 +0700

    Add Datasets

--- src/main/java/ru/sashasuper/logic/Dataset.java
+++ src/main/java/ru/sashasuper/logic/Dataset.java
@@ -1,4 +1,11 @@
 package ru.sashasuper.logic;
 
-public class Dataset {
+
+import java.util.AbstractMap.SimpleEntry;
+import java.util.List;
+
+// Допытливый зритель, конечно, спросит: "Ну и нафига он нужен абстрактным"?
+// Отвечаю: для возможной ленивой инициализации
+public abstract class Dataset {
+    public abstract List<SimpleEntry<Vector, Vector>> getAll();
 }
