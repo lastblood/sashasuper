93a4bdc0a64bc12f4e33fcf355581a65d0ef8986
lastblood <lastblood.xr@gmail.com>
2020-06-10 14:26:36 +0700

    Add IDXReader

---
+++ src/main/java/ru/sashasuper/io/IDXReader.java
@@ -0,0 +1,89 @@
+package ru.sashasuper.io;
+
+import ru.sashasuper.logic.Vector;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class IDXReader {
+    private final String imagesFilePath;
+    private final String labelsFilePath;
+
+    public IDXReader(String imagesFilePath, String labelsFilePath) {
+        this.imagesFilePath = imagesFilePath;
+        this.labelsFilePath = labelsFilePath;
+    }
+
+    private static void thr(boolean condition) {
+        if(condition)
+            throw new IllegalArgumentException();
+    }
+
+    private static void thr(boolean condition, String message) {
+        if(condition)
+            throw new IllegalArgumentException(message);
+    }
+
+    private int readUnsignedInt(InputStream inImage) throws IOException {
+        long temp = (inImage.read() << 24) | (inImage.read() << 16) | (inImage.read() << 8) | (inImage.read());
+        if(temp < 0 || temp > Integer.MAX_VALUE)
+            throw new IllegalArgumentException("Wrong value for unsigned integer: " + temp);
+        return (int) temp;
+    }
+
+    // Сразу переводит в негатив, где 255 - белый
+    private Vector readImageInVector(InputStream inImage, int length) throws IOException {
+        float[] array = new float[length];
+
+        for (int pixel = 0; pixel < length; pixel++)
+            array[pixel] = 255 - inImage.read();
+
+        return new Vector(length, array);
+    }
+
+    private HashMap<Integer, ArrayList<Vector>> read
+            (InputStream imageInput, InputStream labelInput, int pixels, int images) throws IOException {
+
+        HashMap<Integer, ArrayList<Vector>> result = new HashMap<>();
+        for(int i = 0; i < images; i++) {
+            Vector vector = readImageInVector(imageInput, pixels);
+            int label = labelInput.read();
+
+            if(label < 0)
+                throw new IllegalArgumentException("Unexpected end of file");
+
+            if(!result.containsKey(label))
+                result.put(label, new ArrayList<>());
+
+            result.get(label).add(vector);
+
+            if(i % 1000 == 0) System.out.println("Number of images extracted: " + i);
+        }
+        return result;
+    }
+
+    public HashMap<Integer, ArrayList<Vector>> readIdx() throws IOException {
+        try(BufferedInputStream imageInput = new BufferedInputStream(new FileInputStream(imagesFilePath));
+            BufferedInputStream labelInput = new BufferedInputStream(new FileInputStream(labelsFilePath))) {
+
+            int magicNumberImages = readUnsignedInt(imageInput);
+            thr(magicNumberImages != 2051, "Corrupted images file");
+            int magicNumberLabels = readUnsignedInt(labelInput);
+            thr(magicNumberLabels != 2049, "Corrupted labels file");
+
+            int imagesCount = readUnsignedInt(imageInput);
+            int labelsCount = readUnsignedInt(labelInput);
+            thr(labelsCount != imagesCount, "Different labels and images files");
+
+            int rows = readUnsignedInt(imageInput);
+            int columns = readUnsignedInt(imageInput);
+            thr(rows * columns <= 0, "Wrong rows (" + rows + ") or columns (" + columns + ") size");
+
+            return read(imageInput, labelInput, rows * columns, imagesCount);
+        }
+    }
+}
