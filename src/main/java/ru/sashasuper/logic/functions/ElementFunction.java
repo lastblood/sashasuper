ff6e00458d695155ca65d001a80dec4d46e64598
lastblood <lastblood.xr@gmail.com>
2020-06-11 06:07:31 +0700

    Need moar inheritance in Function

---
+++ src/main/java/ru/sashasuper/logic/functions/ElementFunction.java
@@ -0,0 +1,8 @@
+package ru.sashasuper.logic.functions;
+
+
+import java.io.Serializable;
+
+public abstract class ElementFunction implements Serializable {
+    public abstract float process(float value);
+}
