8a07d2bc8389fc665963eb9dac247cbc90cc3ec6
lastblood <lastblood.xr@gmail.com>
2020-06-11 01:22:25 +0700

    Make Matrix immutable again

--- src/main/java/ru/sashasuper/logic/Matrix.java
+++ src/main/java/ru/sashasuper/logic/Matrix.java
@@ -12,30 +12,18 @@ public class Matrix implements Serializable {
         this.values = values;
     }
 
-    public Matrix() {
+    private Matrix() {
     }
 
     public int getRows() {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
-
     public float[][] getValues() {
         return values;
-    }
-
-    public void setValues(float[][] values) {
-        this.values = values;
     }
 }
