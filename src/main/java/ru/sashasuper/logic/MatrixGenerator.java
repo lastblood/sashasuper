8d17a2f3e5733c72d835c8b8ffc85626ad24867f
lastblood <lastblood.xr@gmail.com>
2020-07-08 17:28:07 +0700

    Add MatrixGenerator hierarchy

--- src/main/java/ru/sashasuper/logic/MatrixGenerator.java
+++ src/main/java/ru/sashasuper/logic/MatrixGenerator.java
@@ -1,35 +1,6 @@
 package ru.sashasuper.logic;
 
-import java.util.Random;
-
-public class MatrixGenerator {
-    private Random random = null;
-
-    public MatrixGenerator() {
-    }
-
-    public MatrixGenerator(Random random) {
-        this.random = random;
-    }
-
-    public Matrix randomMatrix(int rows, int columns, float min, float max) {
-        if(rows < 0 || columns < 0)
-            throw new IllegalArgumentException("Rows and columns should be > 0");
-
-        Random r = (random == null) ? new Random() : random;
-
-        float values[][] = new float[rows][columns];
-
-        for(int y = 0; y < rows; y++) {
-            for(int x = 0; x < columns; x++) {
-                values[y][x] = min + r.nextFloat() * (max - min);
-            }
-        }
-
-        return new Matrix(values);
-    }
-
-    public Matrix randomMatrix(int rows, int columns) {
-        return randomMatrix(rows, columns, -1, 1);
-    }
+public abstract class MatrixGenerator {
+    abstract public Matrix generateMatrix(int rows, int columns);
+    abstract public Matrix[] generateMatrices(boolean biased, int ... sizes);
 }
