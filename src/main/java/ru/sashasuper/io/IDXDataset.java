caa280ae0f461827dafb1b50fc917be3c2cae546
lastblood <lastblood.xr@gmail.com>
2020-09-29 20:36:20 +0700

    Refactor IDXDataset

--- src/main/java/ru/sashasuper/io/IDXDataset.java
+++ src/main/java/ru/sashasuper/io/IDXDataset.java
@@ -15,27 +15,35 @@ public class IDXDataset extends Dataset {
     private List<SimpleEntry<Vector, Integer>> lazyList;
     private int outputVectorLength = 10;
 
+    private static final String nulMessage = "List in dataset is null";
+
     public IDXDataset(List<SimpleEntry<Vector, Integer>> lazyList) {
-        thr(lazyList == null, "List in dataset is null");
+        thr(lazyList == null, nulMessage);
         this.lazyList = lazyList;
     }
 
     public IDXDataset(List<SimpleEntry<Vector, Integer>> lazyList, int outputVectorLength) {
-        thr(lazyList == null, "List in dataset is null");
+        thr(lazyList == null, nulMessage);
         this.lazyList = lazyList;
         this.outputVectorLength = outputVectorLength;
     }
 
-    private Vector vectorWithOneAtPosition(Integer value) {
-        float[] vectorValue = new float[outputVectorLength];
+    // Возвращает вектор с нулями на всех позициях кроме [value+1]
+    public static Vector vectorWithOneAtPosition(int value, int length) {
+        thr(value <= length);
+        float[] vectorValue = new float[length];
         vectorValue[value] = 1.0f;
         return new Vector(vectorValue);
+    }
+
+    private Vector vectorWithOneAtPosition(Integer value) {
+        return vectorWithOneAtPosition(value, outputVectorLength);
     }
 
     @Override
     public List<SimpleEntry<Vector, Vector>> getAll() {
         if(processedList == null) {
-            thr(lazyList == null, "List in dataset is null");
+            thr(lazyList == null, nulMessage);
             processedList = new ArrayList<>(lazyList.size());
 
             for (SimpleEntry<Vector, Integer> entry : lazyList)
